2f28f51fb3237636bb180ab07b55924996e85c3d
unknown <joy12665060@gmail.com>
2023-01-01 02:54:33 +0800

    update route

---
+++ src/screen/login.tsx
@@ -0,0 +1,15 @@
+import '../../res/css/login.scss'
+
+const Login = () => {
+    return (
+        <div className='bodyBG'>
+            <div>
+                <p style={{ color: "#FFFFFF" }}>登入畫面</p>
+            </div>
+        </div>
+    )
+
+
+}
+
+export default Login
---
+++ src/screen/slot/SlotAlert.tsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import '../../../res/css/slotAlert.scss'
+
+
+const SlotAlert = () => {
+    // const [count, setCount] = useState(0)
+
+
+
+    return (
+        <div className='bodyBox'>
+            <p style={{ color: "#FFFFFF" }}>老虎機畫面</p>
+
+        </div>
+    )
+
+
+}
+
+export default SlotAlert
